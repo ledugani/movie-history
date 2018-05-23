abaef2ba7f33df17b96169e119f406db1f8a08c5
Tom Dugan <thomas@aadwebb.com>
2018-05-22 20:40:37 -0500

    promise retrieves api key and setter fx for api key

---
+++ javascripts/apiKeys.js
@@ -0,0 +1,27 @@
+const tmdb = require('./tmdb');
+
+const apiKeys = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax('./db/apiKeys.json')
+      .done((data) => {
+        resolve(data.apiKeys);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const retrieveKeys = () => {
+  apiKeys()
+    .then((results) => {
+      tmdb.setKey(results);
+    })
+    .catch((err) => {
+      console.error('no keys:', err);
+    });
+};
+
+module.exports = {
+  retrieveKeys,
+};
